ffc3ce82a0877b75d8e6a5359e7853ab82a8a7e0
Matthew Kartchner <mkartch12@gmail.com>
2019-01-21 17:39:53 +0800

    Prepared for deploy

--- server.js
+++ server.js
@@ -1,38 +1,53 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
 
-const users = require('./routes/api/users');
-const profiles = require('./routes/api/profiles');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profiles = require("./routes/api/profiles");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
 app.use(bodyParser.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
-mongoose.connect(db, {
-    useNewUrlParser: true
-  })
-  .then(() => console.log('MongoDB Connected'))
-  .catch((err) => console.log(err));
+const db = require("./config/keys").mongoURI;
+mongoose
+	.connect(
+		db,
+		{
+			useNewUrlParser: true
+		}
+	)
+	.then(() => console.log("MongoDB Connected"))
+	.catch(err => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Use Routes
-app.use('/api/users', users);
-app.use('/api/profiles', profiles);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profiles", profiles);
+app.use("/api/posts", posts);
+
+// Serve static if in production
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
